986760f6d5c06cf718c9ed9c152e1415b3941902
Sam Garson <samtgarson@gmail.com>
2020-08-26 09:19:57 +0100

    Initial Commit

---
+++ email/Event.swift
@@ -0,0 +1,41 @@
+//
+//  Event.swift
+//  email
+//
+//  Created by Sam Garson on 26/08/2020.
+//  Copyright © 2020 Sam Garson. All rights reserved.
+//
+
+import SwiftUI
+import CoreData
+
+extension Event {
+    static func create(in managedObjectContext: NSManagedObjectContext){
+        let newEvent = self.init(context: managedObjectContext)
+        newEvent.timestamp = Date()
+        
+        do {
+            try  managedObjectContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }   
+}
+
+extension Collection where Element == Event, Index == Int {
+    func delete(at indices: IndexSet, from managedObjectContext: NSManagedObjectContext) {
+        indices.forEach { managedObjectContext.delete(self[$0]) }       
+ 
+        do {
+            try managedObjectContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }
+}
